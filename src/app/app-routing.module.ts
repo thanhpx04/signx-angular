26fb8e5f5f2bd110632b38e06fbec78e0ef5bf12
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-25 15:30:14 +0700

    Authentication Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { ControlComponent } from './control/control.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HistoricalUserComponent } from './historical-user/historical-user.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './shared/auth/auth.guard';
 
 
 const routes: Routes = [
   { path: "", redirectTo: "/dashboard", pathMatch: "full" },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "control", component: ControlComponent },
-  { path: "historical-user", component: HistoricalUserComponent },
+  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: "control", component: ControlComponent, canActivate: [AuthGuard] },
+  { path: "historical-user", component: HistoricalUserComponent, canActivate: [AuthGuard] },
   { path: "login", component: LoginComponent }
 ];
 
